21340b1285f75576122dbf9a6eed3bcbe2aa0cb6
AlexanderPCristobal <b2025apcristobal@pshs.edu.ph>
2026-08-01 16:45:31 +0800

    feat: fixed joshua

--- scripts/interactables/door.gd
+++ scripts/interactables/door.gd
@@ -1,13 +1,4 @@
-extends Area2D
-class_name Interactable
-
-@export var prompt: String = "test"
+extends Interactable
 
 func interact(player: Player) -> void:
 	pass
-	
-func highlight() -> void:
-	get_parent().modulate = Color(1.0, 1.0, 1.0) * 1.5
-
-func darken() -> void:
-	get_parent().modulate = Color(1.0, 1.0, 1.0)

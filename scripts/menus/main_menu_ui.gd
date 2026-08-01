795e6d13c4be041d78d1936e384e68182930c5fc
ajdlanuza <adamjdlanuza@gmail.com>
2026-08-02 02:26:32 +0800

    feat: add the messy house level and a Sprite2D cycler

--- scripts/menus/main_menu_ui.gd
+++ scripts/menus/main_menu_ui.gd
@@ -1,8 +1,8 @@
 extends Control
 
 func _on_start_pressed() -> void:
-	PlayerStats.curr_level = "store"
-	SceneLoader.load_scene(Constants.SCENE_PATHS.store_inside)
+	PlayerStats.curr_level = "messy_house"
+	SceneLoader.load_scene(Constants.SCENE_PATHS.messy_house)
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()

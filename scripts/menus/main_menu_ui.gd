342b7ae97980f985a626ebaeade609490118c9e2
ajdlanuza <adamjdlanuza@gmail.com>
2026-08-02 02:41:39 +0800

    fix: revert menu to play house 1 scene

--- scripts/menus/main_menu_ui.gd
+++ scripts/menus/main_menu_ui.gd
@@ -1,8 +1,8 @@
 extends Control
 
 func _on_start_pressed() -> void:
-	PlayerStats.curr_level = "messy_house"
-	SceneLoader.load_scene(Constants.SCENE_PATHS.messy_house)
+	PlayerStats.curr_level = "start"
+	SceneLoader.load_scene(Constants.SCENE_PATHS.house_1)
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()

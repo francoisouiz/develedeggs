427b09e9f236a25c6a33c26a38f09fb40e6db794
AlexanderPCristobal <b2025apcristobal@pshs.edu.ph>
2026-08-01 14:57:09 +0800

    feat: tweak player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,24 +1,15 @@
 extends CharacterBody2D
 class_name Player
 
-const speed: float = 10
+const speed: float = 60
 
 func _ready() -> void:
 	add_to_group(&"Player")
 	
-	
-
-func _physics_process(delta: float) -> void:
-	var direction_hor: float = Input.get_axis("ui_left", "ui_right")
-	if direction_hor and PlayerStats.can_move:
-		velocity.x = direction_hor * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		
-	var direction_vert: float = Input.get_axis("ui_up", "ui_down")
-	if direction_vert and PlayerStats.can_move:
-		velocity.y = direction_vert * speed
-	else:
-		velocity.y = move_toward(velocity.y, 0, speed)
-		
+func _physics_process(_delta: float) -> void:
+	get_input()
 	move_and_slide()
+
+func get_input() -> void:
+	var input_direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_direction * speed

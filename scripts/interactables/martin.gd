780504ffda8743820a25057b12fdbc289adb9d42
AlexanderPCristobal <b2025apcristobal@pshs.edu.ph>
2026-08-01 21:21:45 +0800

    feat: scene 1 is done.

---
+++ scripts/interactables/martin.gd
@@ -0,0 +1,4 @@
+extends Interactable
+
+func interact(player: Player) -> void:
+	DialogueManager.show_dialogue_balloon(ResourceLoader.load(Constants.DIALOGUE_PATHS.martin), "start")
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,30 @@
 extends CharacterBody2D
 class_name Player
 
+signal entered_photo
+signal exited_photo
+signal load_finished
+
 const speed: float = 60
-# @onready var photo_film: PhotoFilm = $"../PhotoFilmTexture2"
+@onready var photo_film: PhotoFilm = $"../PhotoFilmTexture"
 @onready var interactable_area: Area2D = $InteractableArea
+@onready var label: Label = $Label
+@onready var camera: AudioStreamPlayer2D = $Camera
+@onready var house_1: Node2D = $".."
+var loading_screen: PackedScene = preload(Constants.SCENE_PATHS.loading_screen)
 var nearby_interactables: Array[Interactable] = []
 var target: Interactable = null
 
+
 func _ready() -> void:
 	add_to_group(&"Player")
+	label.hide()
+	house_1.queso_finished.connect(on_queso_finished)
 	
 func _physics_process(_delta: float) -> void:
 	get_nearest_interactable()
 	if target:
+		label.show()
 		target.highlight()
 	if PlayerStats.can_move:
 		get_input()
@@ -23,14 +35,18 @@ func get_input() -> void:
 	velocity = input_direction * speed
 	
 func _input(event: InputEvent) -> void:
-	#if event.is_action_pressed("flip_photo") and PlayerStats.can_move:
-		#match PlayerStats.in_photo:
-			#true:
-				#photo_film.hide()
-				#PlayerStats.in_photo = false
-			#false:
-				#photo_film.show()
-				#PlayerStats.in_photo = true
+	if event.is_action_pressed("flip_photo") and PlayerStats.can_move and PlayerStats.can_photo:
+		if PlayerStats.curr_level == "start" and PlayerStats.scene_progress == 0:
+			return
+		elif PlayerStats.curr_level == "start" and PlayerStats.scene_progress == 1:
+			PlayerStats.can_photo = false
+			DialogueManager.show_dialogue_balloon(ResourceLoader.load(Constants.DIALOGUE_PATHS.queso), "start")
+		elif PlayerStats.curr_level == "start" and PlayerStats.scene_progress == 2:
+			flash()
+			DialogueManager.show_dialogue_balloon(ResourceLoader.load(Constants.DIALOGUE_PATHS.post_photo), "start")
+		else:
+			flash()
+				
 	if event.is_action_pressed("interact") and PlayerStats.can_move:
 		if target:
 			target.interact(self)
@@ -40,6 +56,7 @@ func get_nearest_interactable() -> void:
 	if target:
 		if target not in nearby_interactables:
 			target.darken()
+			label.hide()
 			target = null
 		return
 	
@@ -52,6 +69,27 @@ func get_nearest_interactable() -> void:
 			min_distance = distance
 			nearest = interactable
 	target = nearest
+
+func on_queso_finished() -> void:
+	flash()
+
+func flash() -> void:
+	camera.play()
+	var new_load_screen: CanvasLayer = loading_screen.instantiate()
+	add_child(new_load_screen)
+	await new_load_screen.loading_screen_ready
+	load_finished.connect(new_load_screen._on_load_finished)
+	load_finished.emit()
+	
+	match PlayerStats.in_photo:
+		true:
+			photo_film.hide()
+			PlayerStats.in_photo = false
+			exited_photo.emit()
+		false:
+			photo_film.show()
+			PlayerStats.in_photo = true
+			entered_photo.emit()
 
 func _on_interactable_area_area_entered(area: Area2D) -> void:
 	if area is Interactable:
